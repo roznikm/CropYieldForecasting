8f56f00e8a47f75385471e22b3f939c97676bd5a
roznikm <lysaporth@asb-mac-porthl.local>
2020-04-05 16:30:55 -0500

    processing script for single county

--- corn_forecasting.py
+++ corn_forecasting.py
@@ -37,6 +37,12 @@ modis.head()
 # %% Read and prepare the weather data (PRISM)
 prism = pd.read_csv('./Data/PrismCornProducers.csv')
 prism = prism.drop('.geo', 1)
+prism['system:time_start'] = prism['system:time_start'].multiply(1/1000)
+prism['date'] = pd.to_datetime(prism['system:time_start'], unit='s')
+prism['year'] = pd.DatetimeIndex(prism['date']).year
+prism['month'] = pd.DatetimeIndex(prism['date']).month
+prism['day'] = pd.DatetimeIndex(prism['date']).day
+prism['GEOID'] = prism['GEOID'].astype(str)
 prism.head(10)
 # %%
 corn = pd.read_csv('./Data/CornProduction.csv')
@@ -50,35 +56,67 @@ corn.head(10)
 # %% Select the Aug forecast & data for it 
 forecast_month = 8
 mask_wasde = (wasde['month'] == forecast_month)
-mask_avhrr = (avhrr['month'] <= forecast_month)
+mask_avhrr = (avhrr['month'] <= forecast_month) & (avhrr['year'] < 2000) & (avhrr['year'] > 1981)
 mask_modis = (modis['month'] <= forecast_month)
+mask_prism = (prism['month'] <= forecast_month)
 wasde_selection = wasde.loc[mask_wasde]
 avhrr_selection = avhrr.loc[mask_avhrr]
 modis_selection = modis.loc[mask_modis]
+prism_selection = prism.loc[mask_prism]
+prism_selection.head(10)
 # %% Select a random county
 corn_county = (corn['GEOID'] == '17109')
 avhrr_county = (avhrr_selection['GEOID'] == '17109')
 modis_county = (modis_selection['GEOID'] == '17109')
+prism_county = (prism_selection['GEOID'] == '17109')
 county_corn = corn[corn_county]
 county_avhrr = avhrr_selection[avhrr_county]
 county_modis = modis_selection[modis_county]
-county_corn.head(10)
+county_prism = prism_selection[prism_county]
+county_prism.head(10)
 # %%
-y = county_corn['Value']
-def determinePeriod(x):
+y = county_corn[['year','Value']]
+def determinePeriod(x, type):
     if x <= 15: 
-        return 'a'
+        return 'a' + '_' + type
     if x > 15: 
-        return 'b'
+        return 'b' + '_' + type
 # Avhrr        
-county_avhrr['position'] = county_avhrr['day'].apply(determinePeriod)
+county_avhrr['position'] = county_avhrr['day'].apply(determinePeriod, type='ndvi')
 county_avhrr['period'] = county_avhrr['month'].astype(str) + county_avhrr['position']
 x_avhrr = county_avhrr[['ndvi', 'year', 'period']]
 x_avhrr = x_avhrr.pivot(index='year', columns='period', values='ndvi')
 # Modis
-county_modis['position'] = county_modis['day'].apply(determinePeriod)
+county_modis['position'] = county_modis['day'].apply(determinePeriod, type='ndvi')
 county_modis['period'] = county_modis['month'].astype(str) + county_modis['position']
 x_modis = county_modis[['NDVI', 'year', 'period']]
 x_modis = x_modis.pivot(index='year', columns='period', values='NDVI')
-x_modis.head(10)
+# Prism
+county_prism['position'] = county_prism['day'].apply(determinePeriod, type='')
+county_prism['period'] = county_prism['month'].astype(str) + county_prism['position']
+x_tmean = county_prism[['tmean', 'year', 'period']]
+x_tmean['period'] = x_tmean['period'].apply(lambda x: x + 'tmean')
+x_tmean = x_tmean.pivot(index='year', columns='period', values='tmean')
+
+x_tmean = county_prism[['tmean', 'year', 'period']]
+x_tmean['period'] = x_tmean['period'].apply(lambda x: x + 'tmean')
+x_tmean = x_tmean.pivot(index='year', columns='period', values='tmean')
+
+x_ppt = county_prism[['ppt', 'year', 'period']]
+x_ppt['period'] = x_ppt['period'].apply(lambda x: x + 'ppt')
+x_ppt = x_ppt.pivot(index='year', columns='period', values='ppt')
+
+x_vpdmax = county_prism[['vpdmax', 'year', 'period']]
+x_vpdmax['period'] = x_vpdmax['period'].apply(lambda x: x + 'vpdmax')
+x_vpdmax = x_vpdmax.pivot(index='year', columns='period', values='vpdmax')
+
+# %% Combine the x variables on year
+x_sat = x_modis.append(x_avhrr)
+x_combined = pd.concat([x_sat, x_tmean, x_ppt, x_vpdmax], axis=1, sort=True)
+full_combined = pd.merge(x_combined, y, how='left', on=['year'])
+full_combined.head(10)
+
+# %%
+
+
 # %%
